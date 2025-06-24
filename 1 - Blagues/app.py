26c5c9d50cdc084cea8830d704bd3208f9ff72bb
Clément Rieuneau <clem.rieuneau@gmail.com>
2025-06-24 14:10:00 +0200

    Premier micro-service

---
+++ 1 - Blagues/app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify
+import random
+
+app = Flask(__name__)
+
+# Liste initiale de blagues
+jokes = [
+    { "joke": "Pourquoi un canard est toujours à l'heure ? Parce qu'il est dans l'étang." },
+    { "joke": "Quel est le jeu de cartes préféré des canards ? La coin-che." },
+    { "joke": "Qu'est-ce qui fait Nioc nioc? Un canard qui parle en verlan." },
+    { "joke": "Comment appelle-t-on un canard qui fait du DevOps ? Un DuckOps." }
+]
+
+@app.route("/joke", methods=["GET"])
+def get_joke():
+    """Renvoie une blague aléatoire"""
+    joke = random.choice(jokes)
+    return jsonify(joke), 200
+
+@app.route("/joke", methods=["POST"])
+def post_joke():
+    """Ajoute une nouvelle blague"""
+    data = request.get_json()
+
+    if not data or "joke" not in data:
+        return jsonify({"error": "Champ 'joke' manquant."}), 400
+
+    new_joke = data["joke"].strip()
+    if len(new_joke) < 10:
+        return jsonify({"error": "La blague doit faire au moins 10 caractères."}), 400
+
+    jokes.append({ "joke": new_joke })
+    return jsonify({"message": "Blague ajoutée avec succès."}), 201
+
+@app.route("/jokes", methods=["GET"])
+def get_jokes() :
+    """Renvoie toutes les blagues disponibles"""
+    return jsonify(jokes), 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
